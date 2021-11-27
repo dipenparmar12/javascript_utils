f886784f90d9314c3c5c01de8aae64103f831ef3
Dipen parmar <dipensavji@gmail.com>
2021-11-28 00:03:18 +0530

    5

--- javaScirpt_cheatSheet_my.js
+++ javaScirpt_cheatSheet_my.js
@@ -1,6 +1,29 @@
 console.clear()
 console.log('Time:', new Date())
 
+
+/*
+
+export default Button              -> import Button from './button'
+                                      const Button = require('./button').default
+         
+export const Button                -> import { Button } from './button'
+                                      const { Button } = require('./button')
+         
+export { Button }                  -> import { Button } from './button'
+                                      const { Button } = require('./button')
+         
+module.exports.Button              -> import { Button } from './button'
+                                      const { Button } = require('./button')
+
+module.exports.Button = Button     -> import { Button } from './button'
+                                      const { Button } = require('./button')
+
+module.exports = Button            -> import * as Button from './button'
+                                      const Button = require('./button')
+
+*/
+
 ////--------------------------
 //// use strict
 ////--------------------------
