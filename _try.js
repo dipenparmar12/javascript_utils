94a2483452cf522361a208d334129caa3fcc7b23
Dipen parmar <dipensavji@gmail.com>
2021-12-04 10:48:05 +0530

    Added  Object/Collection unwind method

--- _try.js
+++ _try.js
@@ -1,16 +1,51 @@
 console.clear()
 console.log('Date', new Date().toISOString())
 
-function test(input, output) {
-  return 
+/**
+ * opposite of unwind
+ * @param {Array} arrayOfObjects - array of objects
+ * @param {String} keys to group by
+ * @example
+ * //  var hobbies = [
+ *  //   { name: 'Dipen', hobby: 'cricket' },
+ *  //   { name: 'Dipen', hobby: 'football' },
+ *  //   { name: 'Hitesh', hobby: 'Chess' },
+ *  //   { name: 'Hitesh', hobby: 'Swimming' },
+ *  // ]
+ *  // var persons = groupBy(hobbies, 'hobby', 'hobbies')
+ *  // [
+ *  //   { name: 'Dipen', hobbies: ['cricket', 'football']},
+ *  //   { name: 'Hitesh', hobbies: ['Chess', 'Swimming']},
+ *  // ]
+ */
+function wind(arrayOfObjects, key) {
+  return arrayOfObjects.reduce((acc, cur) => {
+    if (!acc[cur[key]]) {
+      acc[cur[key]] = []
+    }
+    acc[cur[key]].push(cur)
+    return acc
+  }, {})
 }
+var hobbies = [
+  { name: 'Dipen', hobby: 'cricket' },
+  { name: 'Dipen', hobby: 'football' },
+  { name: 'Dipen', hobby: 'Chess' },
+  { name: 'Hitesh', hobby: 'Chess' },
+  { name: 'Hitesh', hobby: 'Swimming' },
+  { name: 'Hitesh', hobby: 'cricket' },
+]
 
 console.log(
-  1
+  '-',
   //
-  // test(people, 'age', 'desc'),
+  // wind(testInventory, 'name'),
+  // wind(hobbies, 'name', 'Name'),
+  wind(hobbies, 'name', 'newName')
 )
-console.warn('')
+
+console.warn('-------------------')
+
 /*    
 
 */
@@ -38,51 +73,51 @@ console.warn('')
 // find='abc'
 // return str.split(find).length - 1
 
-/// Gen Arrays
-function getArr(min) {
-  return new Array(min).fill(0).map((_, i) => i)
-}
+// /// Gen Arrays
+// function getArr(min) {
+//   return new Array(min).fill(0).map((_, i) => i)
+// }
 
-/// Gen random
-function getRandomArr(min, maxVal = 10) {
-  return new Array(min)
-    .fill(0)
-    .map((_, i) => Math.floor(Math.random() * maxVal + 1))
-}
+// /// Gen random
+// function getRandomArr(min, maxVal = 10) {
+//   return new Array(min)
+//     .fill(0)
+//     .map((_, i) => Math.floor(Math.random() * maxVal + 1))
+// }
 
-function shuffle(arr) {
-  let last = arr.length
-  let random
+// function shuffle(arr) {
+//   let last = arr.length
+//   let random
 
-  let rand = (n) => Math.floor(Math.random() * n)
+//   let rand = (n) => Math.floor(Math.random() * n)
 
-  let swap = function (arr, a, b) {
-    ;[arr[a], arr[b]] = [arr[b], arr[a]]
-  }
+//   let swap = function (arr, a, b) {
+//     ;[arr[a], arr[b]] = [arr[b], arr[a]]
+//   }
 
-  while (last > 0) {
-    random = rand(last)
-    swap(arr, --last, random)
-  }
+//   while (last > 0) {
+//     random = rand(last)
+//     swap(arr, --last, random)
+//   }
 
-  return arr
-}
+//   return arr
+// }
 
-// shuffle([10, 15, 8, 6, 2, 4, 17, 20, 13])
-getArr(8) // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-getRandomArr(8, 10)
+// // shuffle([10, 15, 8, 6, 2, 4, 17, 20, 13])
+// getArr(8) // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// getRandomArr(8, 10)
 
-function getRnd(a, n) {
-  return new Array(n)
-    .fill(null)
-    .map(() => a[Math.floor(Math.random() * a.length)])
-}
-//  const arr = myArray
-//       .map((a) => ({sort: Math.random(), value: a}))
-//       .sort((a, b) => a.sort - b.sort)
-//       .map((a) => a.value)
+// function getRnd(a, n) {
+//   return new Array(n)
+//     .fill(null)
+//     .map(() => a[Math.floor(Math.random() * a.length)])
+// }
+// //  const arr = myArray
+// //       .map((a) => ({sort: Math.random(), value: a}))
+// //       .sort((a, b) => a.sort - b.sort)
+// //       .map((a) => a.value)
 
-getRnd([1, 2, 3, 4], 1)
+// getRnd([1, 2, 3, 4], 1)
 
 // let per = {
 //     e: [1,2],
