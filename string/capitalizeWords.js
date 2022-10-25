0370769be1b630490faf94bb0169543d2500afb1
Dipen parmar <dipensavji@gmail.com>
2022-10-25 15:37:02 +0530

    feat(capitalizeWords)

--- string/capitalizeWords.js
+++ string/capitalizeWords.js
@@ -1,7 +1,7 @@
 /**
- *
- * @param str
- * @returns
+ * Capitalize the First Letter of Each Word in JavaScript
+ * @param {String} str
+ * @returns {String} str
  */
 function capitalizeWords(str) {
   return str && str.constructor.name === 'String'
@@ -13,9 +13,41 @@ function capitalizeWords(str) {
     : str
 }
 
-const t = ['one', 'tWo thre', 'trhee FOUR', 'again tTTT', 'aaaa aaaa aaaa']
-t.forEach((el, i) => {
-  console.log(`${t[i]}, ===> `, capitalizeWords(el))
-})
-
 export default capitalizeWords
+
+
+/* 
+========================================================
+  example
+======================================================== 
+
+// const t = ['one', 'tWo thre', 'trhee FOUR', 'again tTTT', 'aaaa aaaa aaaa']
+// t.forEach((el, i) => {
+//   console.log(`${t[i]}, ===> `, capitalizeWords(el))
+// })
+
+*/
+
+
+
+/**
+ * Capitalize the First Letter of Each Word in JavaScript – a JS Uppercase Tutorial
+ * @param {*} str
+ * @returns
+ * @example
+ * capitalizeSentence("abc def ghi") // 'Abc Def Ghi'
+ * capitalizeSentence("abcd_ef") // 'Abcd Ef'
+ *
+ * @see https://www.freecodecamp.org/news/how-to-capitalize-words-in-javascript/#:~:text=In%20JavaScript%2C%20we%20have%20a,%22%3B%20publication%5B0%5D.
+ */
+function capitalizeSentence(str, regex = /[_-]/g, replacer = ' ') {
+  if (!str || !str?.split) return str
+  const sanitizedWords = str.replace(regex, replacer).split(' ')
+
+  return sanitizedWords
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ')
+    .trim()
+}
+
+export capitalizeSentence
