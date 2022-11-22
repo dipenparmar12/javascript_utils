5095f5e25c242edaef826c86b463dbc348e3eac7
Dipen parmar <dipensavji@gmail.com>
2022-11-22 16:11:02 +0530

    feat(Object/flattenV2): Object flatten method.

--- obj/flatten.js
+++ obj/flatten.js
@@ -4,8 +4,7 @@ export default function flatten(data) {
     if (Object(cur) !== cur) {
       result[prop] = cur
     } else if (Array.isArray(cur)) {
-      for (var i = 0, l = cur.length; i < l; i++)
-        recurse(cur[i], prop + '[' + i + ']')
+      for (var i = 0, l = cur.length; i < l; i++) recurse(cur[i], prop + '[' + i + ']')
       if (l === 0) result[prop] = []
     } else {
       var isEmpty = true
@@ -20,20 +19,71 @@ export default function flatten(data) {
   return result
 }
 
-// flattenObject({ type: 'Land', id: 'a086g000000zfUj' })
-// data = {
-//   a: 1,
-//   b: 2,
-//   c: {
-//     c1: 'c11',
-//     c2: 'C22',
-//   },
-//   d: {
-//     d1: 'd444',
-//     d2: {
-//       dd1: 1,
-//       d: false,
-//     },
-//   },
+/**
+ * A function to convert multilevel object to single level object
+ * and use key value pairs as Column and row pairs using recursion
+ * @param {*} data
+ * @returns {VerifyKeyObjectInput}
+ * @see https://github.com/appbaseio/reactivecore/blob/master/src/utils/helper.js#L1074
+ */
+export const flattenV2 = (data) => {
+  const result = {}
+
+  function recurse(cur, prop = '') {
+    if (Object(cur) !== cur) {
+      result[prop] = cur
+    } else if (Array.isArray(cur)) {
+      result[prop] = JSON.stringify(cur)
+    } else {
+      let isEmpty = true
+      Object.keys(cur).forEach((p) => {
+        isEmpty = false
+        recurse(cur[p], prop ? `${prop}.${p}` : p)
+      })
+      if (isEmpty && prop) {
+        result[prop] = {}
+      }
+    }
+  }
+
+  recurse(data)
+
+  return result
+}
+
+/* 
+========================================================
+  Usage/Example 
+======================================================== 
+
+flatten({ type: 'Land', id: 'a086g000000zfUj' })
+data = {
+  a: 1,
+  b: 2,
+  c: {
+    c1: 'c11',
+    c2: 'C22',
+  },
+  d: {
+    d1: 'd444',
+    d2: {
+      dd1: 1,
+      d: false,
+    },
+  },
+}
+
+flattenV2(data)
+// {
+//   "a": 1,
+//   "b": 2,
+//   "c.c1": "c11",
+//   "c.c2": "C22",
+//   "d.d1": "d444",
+//   "d.d2.dd1": 1,
+//   "d.d2.d": false
 // }
-// flattenObject(data)
+
+
+flatten(data)
+*/
