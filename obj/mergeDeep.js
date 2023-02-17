4923f1c0a62a0bddae144f4ce51a7b248e020720
Dipen parmar <dipensavji@gmail.com>
2023-02-17 10:45:23 +0530

    fix(Object/deepMergeV2): RangeError: Maximum call stack size exceeded`

--- obj/mergeDeep.js
+++ obj/mergeDeep.js
@@ -209,60 +209,6 @@ console.log(mergedData)
 
 
 
-// function deepMergeV2(target, source) {
-// // Create a copy of the source object to prevent modification of the original object
-//   const newSource = JSON.parse(JSON.stringify(source));
-
-//   // Loop through each key in the newSource object
-//   for (const key in newSource) {
-//     if (Object.prototype.hasOwnProperty.call(newSource, key)) {
-//       // If the key exists in both objects, recursively merge the values
-//       if (target[key] !== undefined) {
-//         deepMergeV2(target[key], newSource[key]);
-//       } else {
-//         // Otherwise, assign the value of the key in newSource to the key in target
-//         target[key] = newSource[key];
-//       }
-//     }
-//   }
-//   return target;
-// }
-
-// export deepMergeV2
-
-/*
-// Usage Example
-const data = {
-  labels: [],
-  datasets: [
-    {
-      data: [],
-      backgroundColor: ['#a6a6a6', '#ed7d31', '#5b9bd5', '#4472c4', '#ffc000',],
-    },
-  ],
-}
-
-const dataToBeMerged = {
-  labels: ['Water Heating', 'Equipment', 'Heating', 'Lighting', 'Cooling'],
-  datasets: [{data: [100, 200, 300, 500], backgroundColor: []},]
-}
-
-const mergedData = deepMergeV2(data, dataToBeMerged);
-
-console.log(mergedData);
-//  {
-//   labels: ['Water Heating', 'Equipment', 'Heating', 'Lighting', 'Cooling'],
-//   datasets: [
-//     {
-//       data: [100, 200, 300, 500],
-//       backgroundColor: ['#a6a6a6', '#ed7d31', '#5b9bd5', '#4472c4', '#ffc000',],
-//     },
-//   ],
-// }
-*/
-
-
-
 // /**
 //  * Deep merge two objects, merging arrays and their elements recursively.
 //  * @param {Object} object - The target object to merge into.
