720a45ee5470b15635693ece09c779a5cc4987c4
Dipen parmar <dipensavji@gmail.com>
2023-02-17 11:19:45 +0530

    fix(Object/mergeRecursiveV2): Recursively merges the properties of two objects or arrays. If the same key is present in both objects, the value from the target object will overwrite the value from the source object.

--- obj/mergeDeep.js
+++ obj/mergeDeep.js
@@ -49,22 +49,43 @@ function mergeRecursive(target, ...sources) {
 
 export default mergeRecursive
 
-// let merged = mergeRecursive(
-//     {
-//       a: 11,
-//       b: {bb: 11},
-//       c: 11,
-//     },
-//     {b: {bbb: 222}, c: 222}
-// )
-// console.log(merged)
-// merged = {
-//   a: 11,
-//   b: {bb: 11, bbb: 222},
-//   c: 222
-// }
 
-export default mergeDeep
+
+/**
+ * Recursively merges the properties of two objects or arrays. If the same key is present in both objects, the value from
+ * the target object will overwrite the value from the source object.
+ *
+ * @param {Object|Array} source - The source object or array to merge.
+ * @param {Object|Array} target - The target object or array to merge with.
+ * @returns {Object|Array} - The merged object or array.
+ * @example
+ *
+ * const source = {a: [1, 2, 3], b: 111, d: 111};
+ * const target = {a: [3, 4, 5], b: 222, c: 333};
+ *
+ * const merged = mergeRecursiveV2(source, target);
+ * //  {a: [1, 2, 3, 3, 4, 5], b: 222, c: 333, d: 111}
+ *
+ * const mergedArray = mergeRecursiveV2([1, 2, 3], [2, 3, 4]);
+ * // [1, 2, 3, 2, 3, 4]
+ */
+export function mergeRecursiveV2(source, target) {
+  if (Array.isArray(source) && Array.isArray(target)) {
+    return [...source, ...target];
+  } else if (isObject(source) && isObject(target)) {
+    let result = {...source};
+    for (let key in target) {
+      if (key in result) {
+        result[key] = mergeRecursiveV2(result[key], target[key]);
+      } else {
+        result[key] = target[key];
+      }
+    }
+    return result;
+  } else {
+    return target;
+  }
+}
 
 /**
  * Merge one or more object and Clone an object recursively. Array,Date,RegExp,Object,Map,Set
