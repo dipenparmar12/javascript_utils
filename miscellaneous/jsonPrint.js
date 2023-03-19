fed969aed227bf685d27e86222cd7ca8055e133c
Dipen parmar <dipensavji@gmail.com>
2023-03-19 15:35:05 +0530

    fix(Misc/JsonPrint): Fix infinity bug

--- miscellaneous/jsonPrint.js
+++ miscellaneous/jsonPrint.js
@@ -1,22 +1,28 @@
-
 const getCircularReplacer = () => {
-  const seen = new WeakSet();
+  const seen = new WeakSet()
   return (key, value) => {
     if (typeof value === 'object' && value !== null) {
       if (seen.has(value)) {
-        return;
+        return
       }
-      seen.add(value);
+      seen.add(value)
     }
-    return value;
-  };
-};
-
+    return value
+  }
+}
 
 let obj = {
-  a: 1
-  data: obj
+  a: 1,
+  data: obj,
 }
 
 let print = JSON.stringify(obj, getCircularReplacer(), 2)
 console.log(print)
+
+/* 
+========================================================
+const obj = {a:1}
+obj.b = obj
+const data = JSON.stringify(obj, getCircularReplacer(), 1)
+======================================================== 
+*/
