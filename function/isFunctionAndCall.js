d347b87a38f3fa8d0ff819b5e656d7e853eee031
Dipen parmar <dipensavji@gmail.com>
2023-03-19 15:33:16 +0530

    feat(Function/isFunctionAndCallV2): add isFunctionAndCall V2

--- function/isFunctionAndCall.js
+++ function/isFunctionAndCall.js
@@ -1,14 +1,27 @@
 /**
+ * Determine whether the given value is a function.
+ * @param {*} x
+ * @param callMethod
+ * @param args
+ * @returns {boolean}
+ */
+export default function isFunction(x, callMethod = false, ...args) {
+  const is = Object.prototype.toString.call(x) === '[object Function]'
+  if (is && callMethod) return x(...args)
+  return is
+}
+
+/**
  *
  * @param {*} func
  * @src https://javascript.info/currying-partials
- * @example isFunctionAndCall((e)=> console.log(e))('curry function test..')
+ * @example isFunctionAndCallV2((e)=> console.log(e))('curry function test..')
  * @example const add = (a, b) => a + b;
  * const add5 = add.bind(null, 5);
  * add5(2) // 7
  */
-export default function isFunctionAndCall(func) {
-  console.log('IsFunctionAndCall.js::[2] func', func)
+export  function isFunctionAndCallV2(func) {
+  console.log('IsFunctionAndCallV2.js::[2] func', func)
   if (Object.prototype.toString.call(func) !== '[object Function]') {
     return () => false
   }
