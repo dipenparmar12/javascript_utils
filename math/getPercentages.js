b3f6a0cf2c0f5f5ea12fcb9473b8b8bfa6c7a9d9
Dipen parmar <dipensavji@gmail.com>
2023-03-19 15:32:54 +0530

    feat(Math/getPercentages): add getPercentages v2

--- math/getPercentages.js
+++ math/getPercentages.js
@@ -24,3 +24,47 @@ const data = {
 };
 data.percentages = getPercentages(data.data, 2); // [9.09, 18.18, 27.27, 45.45]
 */
+
+
+/**
+ * Returns an array of percentages that correspond to each value in the input data array.
+ * @param {number[]} dataSet - The input data array to calculate percentages for.
+ * @param {number} [decimals=0] - The number of decimal places to round the percentages to.
+ * @param {Boolean} [precise=true] - A boolean value indicating whether the function should ensure that the sum of the calculated percentages is exactly 100% of the total, rather than allowing it to exceed 100% when the decimal values are rounded
+ * @returns {number[]} An array of percentages with one element for each value in the input data array.
+ * @example
+ * const data = {
+ *   data: [100, 200, 300, 500],
+ *   percentages: [],
+ * };
+ * data.percentages = getPercentages(data.data, 2); // [9.09, 18.18, 27.27, 45.45]
+ */
+function getPercentagesV2(dataSet, decimals = 1, precise = true) {
+  const sum = dataSet.reduce((acc, curr) => acc + curr, 0)
+  let percentages = dataSet.map((value) =>
+    Number(((value / sum) * 100).toFixed(decimals)),
+  )
+
+  if (precise) {
+    const roundedSum = percentages.reduce((acc, curr) => acc + curr, 0)
+    if (roundedSum > 100) {
+      const diff = 100 - roundedSum
+      const largestValue = Math.max(...percentages)
+      return percentages.reduce((acc, currItem, idx) => {
+        acc[idx] = currItem
+        if (currItem === largestValue) {
+          acc[idx] += diff
+        }
+        return acc
+      }, [])
+    }
+    // if (roundedSum < 0) {
+    //   // TODO::if required
+    // }
+  }
+
+  return percentages
+}
+
+export default getPercentagesV2
+
