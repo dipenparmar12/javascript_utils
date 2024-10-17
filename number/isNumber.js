911b41d853bdfe4d7425dc737cf6486b80cacebf
Dipen parmar <dipensavji@gmail.com>
2024-10-17 12:37:16 +0530

    Refactor isNumber function to include support for decimal numbers

--- number/isNumber.js
+++ number/isNumber.js
@@ -4,3 +4,5 @@
  * @see https://github.com/rsuite/rsuite/blob/main/src/InputNumber/InputNumber.tsx#L66
  */
 const isNumber = (value) => /(^-?|^\+?|^\d?)\d*\.\d+$/.test(value + '')
+
+export default isNumber
